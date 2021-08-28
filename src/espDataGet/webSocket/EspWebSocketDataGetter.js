280688bc34b19c423ea6b2e58ebefb25f4f36ec8
CJ <marcin.ochocki93@gmail.com>
2021-08-28 14:43:24 +0200

    checking if webSocket is still connected

--- src/espDataGet/webSocket/EspWebSocketDataGetter.js
+++ src/espDataGet/webSocket/EspWebSocketDataGetter.js
@@ -4,6 +4,8 @@ export class EspWebSocketDataGetter {
     this.store = store;
     this.webSocketPath = webSocketPath_;
     this.webSocket = null;
+    this.recentMessageTime = 0;
+    this.checkConnectionInterval = null;
   }
 
   initializeWebSocket() {
@@ -11,8 +13,11 @@ export class EspWebSocketDataGetter {
     this.webSocket = new WebSocket(this.webSocketPath);
     this.webSocket.onopen = function() {
       console.log("Initialized WebSocket connection: " + self.webSocketPath);
+      self.recentMessageTime = new Date().getTime();
+      self.setCheckConnectionInterval();
     };
     this.webSocket.onmessage = function (event) {
+      self.recentMessageTime = new Date().getTime();
       if (event.data != null) {
         let json = JSON.parse(event.data);
         if (json == null) {
@@ -33,11 +38,20 @@ export class EspWebSocketDataGetter {
         }
       }
     };
+    this.webSocket.onerror = function(event) {
+      console.error("WebSocket error observed:", event);
+    };
   }
 
   closeWebSocket() {
+    if (this.webSocket == null || this.webSocket.readyState !== WebSocket.OPEN) {
+      return;
+    }
+    
     this.webSocket.close();
     this.webSocket = null;
+    this.clearCheckConnectionInterval();
+    console.log("Closed WebSocket connection");
   }
 
   updateWebSocketPath(newWebSocketPath) {
@@ -47,5 +61,23 @@ export class EspWebSocketDataGetter {
 
     this.webSocketPath = newWebSocketPath;
     this.initializeWebSocket();
+  }
+
+  setCheckConnectionInterval() {
+    let self = this;
+    this.checkConnectionInterval = setInterval(
+      function() {
+        if (new Date().getTime() - self.recentMessageTime > 5000) {
+          console.log("Exceeded time waiting for next WebSocket message");
+          self.closeWebSocket();
+          self.store.commit('espConnect/setConnected', false);
+        }
+      },
+      1000
+    )
+  }
+
+  clearCheckConnectionInterval() {
+    clearInterval(this.checkConnectionInterval);
   }
 }
